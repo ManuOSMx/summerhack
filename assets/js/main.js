c841cd3afdca57477acee6c750e686d065067abb
Manuel Ortiz <manuel4612@hotmail.com>
2021-07-30 15:30:57 -0500

    first web

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+var editSaludo = document.getElementById("editar");
+var saludo = "Innovaccion Virtual";
+editSaludo.innerHTML = "Hola a todas y todos, los saludan desde el hackathon " + saludo + " ";
+console.log(saludo);
---
+++ assets/js/main.ts
@@ -0,0 +1,7 @@
+const editSaludo = document.getElementById("editar");
+
+var saludo : String = "Innovaccion Virtual";
+editSaludo.innerHTML = `Hola a todas y todos, los saludan desde el hackathon ${saludo} `
+
+
+console.log(saludo);
